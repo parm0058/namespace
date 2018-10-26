855bc5c7984fdddfaef303b2d8ac2deca7a818e8
parm0058 <43075509+parm0058@users.noreply.github.com>
2018-10-26 17:12:52 -0400

    Add files via upload

--- namespace.js
+++ namespace.js
@@ -1,34 +1,35 @@
 var PARM0058 = {
-    
-init: function(){
-    let div = document.createElement("div");
-    div.className = "box";
-    div.textContent = "parm0058";
-    let getboxes = document.getElementById("boxes");
-    getboxes.appendChild(createNewDiv);
-    
-    
-    div.addEventListener("click", function(){
-        
-        div.style.borderColor = "blue";
-        div.style.backgroundColor = "lightblue";
-        div.style.Color = "hotgreen";
-        
-         });
-    
-    //mouse overevent
-    
-    div.addEventListener("mouseover", function(){
-    div.classList.toggle("highlight");
-        
-    });
-    
-    //mouse outevnt
-    
-    div.addEventListener("mouseover", function(){
-     div.classList.toggle("highlight");
-        div.removeAttribute('style');
-    });
-    
-}
+
+    init: function () {
+        let div = document.createElement("div");
+        div.className = "box"; // add "box" class to div
+        div.textContent = "parm0058"; //add text content to div
+        let getboxes = document.getElementById("boxes"); //get div of id boxes
+        getboxes.appendChild(div); //apped div with box class to the div with id boxes
+
+        //click event
+        div.addEventListener("click", function () {
+
+            div.style.borderColor = "blue";
+            div.style.backgroundColor = "lightblue";
+            //createNewDiv.style.Color = "hotyellow";
+
+        });
+
+        //mouse overevent
+
+        div.addEventListener("mouseover", function () {
+            div.classList.toggle("highlight");
+
+        });
+
+        //mouse outevnt
+
+        div.addEventListener("mouseout", function () {
+            div.classList.toggle("highlight");
+            div.removeAttribute('style');
+        });
+
+
+    }
 };
